13cc5e47ccda55fb7acf35a08b1b0bd9b42914a3
sirSlappz <87740039+sirSlappz@users.noreply.github.com>
2022-03-31 15:21:28 -0500

    user sign up working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get "pages/home"
   root "pages#home"
   resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
