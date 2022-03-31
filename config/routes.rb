5ce555da523b1660cef9490aab9520c75e70bb4f
sirSlappz <87740039+sirSlappz@users.noreply.github.com>
2022-03-31 17:56:01 -0500

    auth helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   root "pages#home"
   resources :articles
   get "signup", to: "users#new"
   resources :users, except: [:new]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  get "logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
